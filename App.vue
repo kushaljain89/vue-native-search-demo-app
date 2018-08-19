f2dbd39240ec61e34aab0a911661db4bcb4d29c3
Kushal Jain <jain.kushal89@gmail.com>
2018-08-19 21:47:48 +0530

    Basic Setup  - Setup Native Base  - Linking using react-native link

--- App.vue
+++ App.vue
@@ -1,11 +1,14 @@
 <template>
-  <main/>
+    <main/>
 </template>
 
 <script>
     import Vue from "vue-native-core"
+    import {VueNativeBase} from "native-base"
     import store from './js/store/index.js'
     import Main from './js/Main'
+
+    Vue.use(VueNativeBase)
 
     export default {
         name: "App",
